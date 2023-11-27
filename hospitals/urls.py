b9f2348a9290ca3539d10ed071748af71d04b986
wingxel <wingxel@gmail.com>
2023-11-27 22:32:29 +0300

    Added add-note url

--- hospitals/urls.py
+++ hospitals/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path
-from hospitals.views import NurseView
+from hospitals.views import NurseView, AddNoteView
 
 app_name = "hospitals"
 
 urlpatterns = [
-    path("nurse/", NurseView.as_view(), name="nurse-index")
+    path("nurse/", NurseView.as_view(), name="nurse-index"),
+    path("add-note/<int:patient_id>/", AddNoteView.as_view(), name="add-note")
 ]
