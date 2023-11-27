a3b6713aa827a5c22d2c72946e5be735ec117f88
wingxel <wingxel@gmail.com>
2023-11-28 01:45:44 +0300

    Added NoteList url

--- hospitals/urls.py
+++ hospitals/urls.py
@@ -1,10 +1,11 @@
 from django.urls.conf import path
-from hospitals.views import NurseView, AddNoteView, NoteDetails
+from hospitals.views import NurseView, AddNoteView, NoteDetails, NotesList
 
 app_name = "hospitals"
 
 urlpatterns = [
     path("nurse/", NurseView.as_view(), name="nurse-index"),
     path("add-note/<int:patient_id>/", AddNoteView.as_view(), name="add-note"),
-    path("note-details/<int:pk>/", NoteDetails.as_view(), name="note-details")
+    path("note-details/<int:pk>/", NoteDetails.as_view(), name="note-details"),
+    path("notes/", NotesList.as_view(), name="notes-list")
 ]
