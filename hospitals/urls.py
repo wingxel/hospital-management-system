6e6e7d4cf07a066068a6e07ccb26781d2f6929f6
wingxel <wingxel@gmail.com>
2023-11-28 00:48:21 +0300

    Added details URL

--- hospitals/urls.py
+++ hospitals/urls.py
@@ -1,9 +1,10 @@
 from django.urls.conf import path
-from hospitals.views import NurseView, AddNoteView
+from hospitals.views import NurseView, AddNoteView, NoteDetails
 
 app_name = "hospitals"
 
 urlpatterns = [
     path("nurse/", NurseView.as_view(), name="nurse-index"),
-    path("add-note/", AddNoteView.as_view(), name="add-note")
+    path("add-note/<int:patient_id>/", AddNoteView.as_view(), name="add-note"),
+    path("note-details/<int:pk>/", NoteDetails.as_view(), name="note-details")
 ]
